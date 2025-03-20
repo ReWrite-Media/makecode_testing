9aa85c11ec533360ddaae11cdd39816b3dcab6f9
neonerz <gerald@rewrite.media>
2025-03-20 08:34:42 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+/**
+ * Custom blocks
+ */
+namespace makecodeTest {
+    /**
+     * count to 20
+     */
+    //% block="count to 20" blockId=agentdropallitemsindividually
+    export function count_to_20() {
+        for (let i = 0; i < 0; i++) {
+            player.execute("say "+i);
+        }
+    }
+}
