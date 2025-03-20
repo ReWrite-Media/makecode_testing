175d2bdf89f94d222c5c09ca66cde832698d02b6
neonerz <gerald@rewrite.media>
2025-03-20 08:43:50 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace makecodeTest {
     /**
      * count to 20
      */
-    //% block="count to 20" blockId=agentdropallitemsindividually
+    //% block="count to 20"
     export function count_to_20() {
         for (let i = 0; i < 0; i++) {
             player.execute("say "+i);
