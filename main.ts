c1d7f667f13aea113a536b6610bf97ec36b3eeef
neonerz <gerald@rewrite.media>
2025-03-20 08:49:59 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,9 +6,16 @@ namespace makecodeTest {
      * count to 20
      */
     //% block="count to 20"
-    export function count_to_20() {
+    export function count_to_20(): void {
         for (let i = 0; i < 0; i++) {
-            player.execute("say "+i);
+            player.execute("say " + i);
         }
+    }
+    /**
+     * say hi
+     */
+    //% block="say hi"
+    export function say_hi(): void {
+        player.execute("say hi");
     }
 }
