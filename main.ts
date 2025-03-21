9543674bd2ea042840f84fc61041e018ce1bff37
neonerz <gerald@rewrite.media>
2025-03-21 08:48:44 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,4 +18,15 @@ namespace makecodeTest {
     export function say_hi(): void {
         player.execute("say hi");
     }
+    /**
+     * place blocks stacked
+     */
+    //% block="place blocks at $x $y $z"
+    //% x.defl=0 y.defl=0 z.defl=0
+    export function place_blocks(x: number, y: number, z: number) {
+        for (let i = y; i < 20; i++) {
+            player.execute("block pos" + [x, i, z])
+            blocks.place(STONE, pos(x, i, z))
+        }
+    }
 }
